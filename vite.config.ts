d654a859708a5d79397e8018bf86ac263c6c665f
Pedro Taiette <pedrolibrais02@gmail.com>
2025-10-26 22:15:27 -0300

    gh_pages_impl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/pato-primordial/"
+  base: isGitHubPages ? "/pato_primordial/" : "",
 });
